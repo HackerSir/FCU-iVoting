f5ba5ab16ef1f07f0348d60d7c82fa325cc346df
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-07-03 21:10:14 +0800

    Create getter and setter of Setting

--- app/Setting.php
+++ app/Setting.php
@@ -9,5 +9,19 @@ class Setting extends Model
     protected $table = 'settings';
     protected $fillable = ['id', 'desc', 'data'];
 
+    static public function get($id)
+    {
+        $setting = self::find($id);
+        if (!$setting) {
+            return null;
+        }
+        return $setting->data;
+    }
 
+    static public function set($id, $data)
+    {
+        $setting = self::find($id);
+        $setting->data = $data;
+        $setting->save();
+    }
 }
