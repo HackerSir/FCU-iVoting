cf81a8ef3dc114dc59140613c219935215e3311b
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-07-07 23:05:46 +0800

    Fix password resets table

---
+++ database/migrations/2015_07_07_230348_fix_password_resets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixPasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('password_resets', function ($table) {
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('password_resets', function ($table) {
+            $table->dropColumn('updated_at');
+        });
+    }
+}
