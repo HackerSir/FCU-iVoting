2b485a2668bf742f6c15af09e2f0542194185777
Chan, Danny <danny50610@yahoo.com.tw>
2015-07-07 17:59:34 +0800

    Format code

--- resources/views/vote/event/show-eas.blade.php
+++ resources/views/vote/event/show-eas.blade.php
@@ -13,10 +13,11 @@
 
             <p>活動期間：{{ $voteEvent->open_time }} &nbsp; ~ &nbsp; {{ $voteEvent->close_time }}</p>
         </div>
+
         <div class="bs-callout bs-callout-warning">
             <h4>投票規則</h4>
             <ul>
-                <li>每人最多可以投&nbsp;{{ $voteEvent->max_selected }}&nbsp;票<br /></li>
+                <li>每人最多可以投&nbsp;{{ $voteEvent->max_selected }}&nbsp;票<br/></li>
                 <li>採相對多數決(也就是最高票獲選)</li>
             </ul>
         </div>
