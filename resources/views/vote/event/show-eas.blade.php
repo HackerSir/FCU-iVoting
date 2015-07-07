d9bd206c453010138924e9a43e69e1ee725c5345
Chan, Danny <danny50610@yahoo.com.tw>
2015-07-07 16:57:55 +0800

    Add vote event Jumbotron

--- resources/views/vote/event/show-eas.blade.php
+++ resources/views/vote/event/show-eas.blade.php
@@ -6,6 +6,12 @@
 
 @section('content')
     <div class="container">
+        <div class="jumbotron">
+            <h1>{{ $voteEvent->subject }}</h1>
 
+            <p>{!! Markdown::parse(htmlspecialchars($voteEvent->info)) !!}</p>
+
+            <p>活動期間：{{ $voteEvent->open_time }} &nbsp; ~ &nbsp; {{ $voteEvent->close_time }}</p>
+        </div>
     </div>
 @endsection
