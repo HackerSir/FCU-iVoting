bd4b09904cdf6bf63409026d4bad9bb14190871c
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-07-09 17:03:41 +0800

    Hide empty set of navbar

--- resources/views/common/navbar_set.blade.php
+++ resources/views/common/navbar_set.blade.php
@@ -1,15 +1,17 @@
 @foreach ($navbar as $name => $uri)
     @if(is_array($uri))
-        {{-- 次級選單 --}}
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ (Auth::check())?str_replace('%user%',Auth::user()->getNickname(),$name):$name }}<span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-                @foreach ($uri as $subName => $subUri)
-                    {{-- 子項目--}}
-                    @include('common.navbar_submenu', ['name' => $subName, 'items' => $subUri])
-                @endforeach
-            </ul>
-        </li>
+        @if(count($uri))
+            {{-- 次級選單 --}}
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ (Auth::check())?str_replace('%user%',Auth::user()->getNickname(),$name):$name }}<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    @foreach ($uri as $subName => $subUri)
+                        {{-- 子項目--}}
+                        @include('common.navbar_submenu', ['name' => $subName, 'items' => $subUri])
+                    @endforeach
+                </ul>
+            </li>
+        @endif
     @else
         {{-- 一般項目 --}}
         @include('common.navbar_item', ['uri' => $uri, 'name' => $name])
