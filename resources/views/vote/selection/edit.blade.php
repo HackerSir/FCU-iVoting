e9c2673fe75026055f89347b5b8aa4f4f46a88e2
Chan, Danny <danny50610@yahoo.com.tw>
2015-07-22 22:07:59 +0800

    Change edit selection style

--- resources/views/vote/selection/edit.blade.php
+++ resources/views/vote/selection/edit.blade.php
@@ -5,52 +5,52 @@
 @endsection
 
 @section('content')
-    <div class="container" style="min-height: 600px">
+    <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">編輯投票選項</div>
-                    {{-- Panel body --}}
-                    <div class="panel-body">
-                        <div class="row">
-                            {!! Form::open(['route' => ['vote-selection.update', $voteSelection->id], 'class' => 'form-horizontal', 'method' => 'PUT']) !!}
-                                <div class="form-group">
-                                    <label class="control-label col-md-3" for="creator">投票活動</label>
-                                    <div class="col-md-9">
-                                        {!! HTML::linkRoute('vote-event.show', $voteSelection->voteEvent->subject, $voteSelection->voteEvent->id, null) !!}
-                                    </div>
-                                </div>
-                                <div class="form-group has-feedback{{ ($errors->has('title'))?' has-error':'' }}">
-                                    <label class="control-label col-md-3" for="title">選項內容</label>
-                                    <div class="col-md-9">
-                                        {!! Form::text('title', $voteSelection->getTitle(), ['id' => 'title', 'placeholder' => '請輸入選項內容', 'class' => 'form-control']) !!}
-                                        @if($errors->has('title'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
-                                        <span class="label label-danger">{{ $errors->first('title') }}</span>@endif
-                                    </div>
-                                </div>
-                                <div class="form-group has-feedback{{ ($errors->has('image'))?' has-error':'' }}">
-                                    <label class="control-label col-md-3" for="image">圖片網址</label>
-                                    <div class="col-md-9">
-                                        {!! Form::textarea('image',  $voteSelection->getImageLinkstext(), ['id' => 'image', 'placeholder' => '請輸入圖片連結，每行一個網址', 'class' => 'form-control', 'rows' => 5]) !!}
-                                        @if($errors->has('image'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
-                                        <span class="label label-danger">{{ $errors->first('image') }}</span>@endif
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-md-12">
-                                    {!! Form::file('image_upload', ['id' => 'image_upload', 'class' => 'form-control', 'accept' => 'image/*', 'multiple']) !!}
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-md-10 col-md-offset-1 text-center">
-                                        <hr />
-                                        {!! Form::submit('修改資料', ['class' => 'btn btn-primary']) !!}
-                                        {!! HTML::linkRoute('vote-event.show', '返回', $voteSelection->voteEvent->id, ['class' => 'btn btn-default']) !!}
-                                    </div>
-                                </div>
-                            {!! Form::close() !!}
+                <div class="well bs-component">
+                    {!! Form::open(['route' => ['vote-selection.update', $voteSelection->id], 'class' => 'form-horizontal', 'method' => 'PUT']) !!}
+                    <fieldset>
+                        <legend>編輯投票選項</legend>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label" for="creator">投票活動</label>
+
+                            <div class="col-md-10 form-control-static">
+                                {!! HTML::linkRoute('vote-event.show', $voteSelection->voteEvent->subject, $voteSelection->voteEvent->id, null) !!}
+                            </div>
                         </div>
-                    </div>
+                        <div class="form-group has-feedback{{ ($errors->has('title'))?' has-error':'' }}">
+                            <label class="col-md-2 control-label" for="title">選項內容</label>
+
+                            <div class="col-md-10">
+                                {!! Form::text('title', $voteSelection->getTitle(), ['id' => 'title', 'placeholder' => '請輸入選項內容', 'class' => 'form-control']) !!}
+                                @if($errors->has('title'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
+                                <span class="label label-danger">{{ $errors->first('title') }}</span>@endif
+                            </div>
+                        </div>
+                        <div class="form-group has-feedback{{ ($errors->has('image'))?' has-error':'' }}">
+                            <label class="col-md-2 control-label" for="image">圖片網址</label>
+
+                            <div class="col-md-10">
+                                {!! Form::textarea('image',  $voteSelection->getImageLinkstext(), ['id' => 'image', 'placeholder' => '請輸入圖片連結，每行一個網址', 'class' => 'form-control', 'style' => 'resize: vertical;', 'rows' => 5]) !!}
+                                @if($errors->has('image'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
+                                <span class="label label-danger">{{ $errors->first('image') }}</span>@endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                {!! Form::file('image_upload', ['id' => 'image_upload', 'class' => 'form-control', 'accept' => 'image/*', 'multiple']) !!}
+                            </div>
+                        </div>
+                        <hr/>
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-1 text-center">
+                                {!! Form::submit('修改資料', ['class' => 'btn btn-primary']) !!}
+                                {!! HTML::linkRoute('vote-event.show', '返回', $voteSelection->voteEvent->id, ['class' => 'btn btn-default']) !!}
+                            </div>
+                        </div>
+                    </fieldset>
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
