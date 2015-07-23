8b56ccd3995c1e6cbebcc80042cbd8cdfded9bb5
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-07-23 15:41:31 +0800

    Create Organizer model

---
+++ app/Organizer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organizer extends Model
+{
+    protected $table = 'organizers';
+    protected $fillable = ['name', 'url', 'logo_url'];
+}
