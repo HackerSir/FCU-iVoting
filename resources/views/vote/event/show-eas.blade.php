f7ee4c3de6de7a2f4dd8035d86303be9eb52ae2f
Chan, Danny <danny50610@yahoo.com.tw>
2015-07-26 20:37:46 +0800

    Remove image tip (Because cursor of image is 'pointer')

--- resources/views/vote/event/show-eas.blade.php
+++ resources/views/vote/event/show-eas.blade.php
@@ -135,7 +135,7 @@
                                                 @if(count($voteSelectionItem->getImageLinks()) > 1)
                                                     <div class="more-image-fake-shadow img-rounded"></div>
                                                 @endif
-                                                <img src="{{ App\Imgur::thumbnail($voteSelectionItem->getImageLinks()[0], 'm') }}" class="img-rounded" style="max-width:100%;max-height:300px;width:auto;height:auto; cursor: pointer;" title="點此看更多圖" data-toggle="modal" data-target="#imageModal" data-title="{{ $voteSelectionItem->getTitle() }}" data-images="{{ implode(';',$voteSelectionItem->getImageLinks()) }}"/>
+                                                <img src="{{ App\Imgur::thumbnail($voteSelectionItem->getImageLinks()[0], 'm') }}" class="img-rounded" style="max-width:100%;max-height:300px;width:auto;height:auto; cursor: pointer;" data-toggle="modal" data-target="#imageModal" data-title="{{ $voteSelectionItem->getTitle() }}" data-images="{{ implode(';',$voteSelectionItem->getImageLinks()) }}"/>
                                             @else
                                                 <img data-src="holder.js/300x300?text=沒有圖片&size=45" class="img-rounded" style="max-width:100%;max-height:300px;width:auto;height:auto;"/>
                                             @endif
