c37c366266e421400ea83ae17cf42c34079472b1
Chan, Danny <danny50610@yahoo.com.tw>
2015-07-26 20:33:16 +0800

    Remove unnecessary css style

--- resources/views/vote/event/show-eas.blade.php
+++ resources/views/vote/event/show-eas.blade.php
@@ -220,7 +220,7 @@
         </div>
     </div>
 
-    <div class="modal fade bs-example-modal-lg" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+    <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
