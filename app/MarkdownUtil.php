4a4af9d58e3308af3708694d2ebead840339716d
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-08-05 23:44:17 +0800

    Finished Markdown auto-newline

--- app/MarkdownUtil.php
+++ app/MarkdownUtil.php
@@ -8,13 +8,14 @@ class MarkdownUtil
 {
     static public function translate($string)
     {
-        return self::autoNewline(Markdown::parse(htmlspecialchars($string)));
-    }
-
-    static private function autoNewline($string)
-    {
-        $string = preg_replace('/<br \/>/', PHP_EOL, $string);
-        $string = nl2br($string);
-        return $string;
+        $result = "";
+        //逐行處理
+        foreach (preg_split("/((\r?\n)|(\r\n?))/", $string) as $line) {
+            //令每行都以兩個空格結尾
+            $line = preg_replace('/\s*($)/', '  $1', $line);
+            $result .= $line . PHP_EOL;
+        }
+        $result = Markdown::parse($result);
+        return $result;
     }
 }
