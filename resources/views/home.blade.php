1acd072effdb6883f940435a9d23b0f4b4cbd9b7
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-08-05 18:11:25 +0800

    Update background and home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,11 +11,18 @@
 @section('main-jumbotron')
     <div style="padding-top: 64px;">
         <div class="container">
-            <h1>逢甲票選系統</h1>
+            <div class="row">
+                <div class="col-md-8 col-sm-8 jumbotron">
+                    <h1>逢甲票選系統</h1>
 
-            <p>一個由學生社團做的票選系統，快來參加各種票選活動吧！！！</p>
+                    <p>一個由學生社團做的票選系統，快來參加各種票選活動吧！！！</p>
 
-            <p><a href="{{ URL::route('vote-event.index') }}" class="btn btn-primary btn-lg">查看票選活動 »</a></p>
+                    <p><a href="{{ URL::route('vote-event.index') }}" class="btn btn-primary btn-lg">查看票選活動 »</a></p>
+                </div>
+                <div class="col-md-4 col-sm-4 hidden-xs">
+                    <img src="{{ asset('pic/logo.png') }}" style="float: right; height: 400px" />
+                </div>
+            </div>
         </div>
     </div>
 @endsection
