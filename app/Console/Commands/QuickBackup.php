570ad949b4364f0ab36a58391ffd12b44601d859
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-08-06 16:03:30 +0800

    Create quick-backup command

---
+++ app/Console/Commands/QuickBackup.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+
+class QuickBackup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:quick-backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Quick backup';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $now = Carbon::now();
+        $filename = $now->format('Ymd_His') . '.sql';
+
+        // database, destination, destinationPath, compression
+        $this->call('db:backup', [
+            '--database' => 'mysql',
+            '--destination' => 'local',
+            '--destinationPath' => $filename,
+            '--compression' => 'null',
+        ]);
+    }
+}
