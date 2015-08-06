018777895d807a8bc74ab09d3d689b516a0561eb
Chan, Danny <danny50610@yahoo.com.tw>
2015-08-06 23:33:26 +0800

    Fix UI (SORRY!)

--- resources/views/vote/event/show-eas.blade.php
+++ resources/views/vote/event/show-eas.blade.php
@@ -32,7 +32,7 @@
         }
 
         @media
-        only screen and (max-width: 479px) {
+        only screen and (max-width: 768px) {
             .jumbotron {
                 padding-left: 10px !important;
                 padding-right: 10px !important;
