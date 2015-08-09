c12861514cc53ae9fc90caf00d00e663ba4e31ec
Chan, Danny <danny50610@yahoo.com.tw>
2015-08-09 13:08:34 +0800

    Remove "contaniner-mobile" css class

--- resources/views/vote/organizer/list.blade.php
+++ resources/views/vote/organizer/list.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <div class="container container-mobile">
+    <div class="container">
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-default">
