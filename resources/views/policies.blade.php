5089a9fc410755f3180acf3d33f0eee126e33c91
Chan, Danny <danny50610@yahoo.com.tw>
2015-08-11 18:57:24 +0800

    Add Tab in policies page

--- resources/views/policies.blade.php
+++ resources/views/policies.blade.php
@@ -1,3 +1,14 @@
 @extends('app')
 
-
+@section('content')
+    <div class="container container-background">
+        <ul class="nav nav-tabs">
+            <li role="presentation" @if(Request::is('policies/privacy')) class="active" @endif>
+                <a href="{{ URL::route('policies.privacy') }}">隱私權</a>
+            </li>
+            <li role="presentation" @if(Request::is('policies/terms')) class="active" @endif>
+                <a href="{{ URL::route('policies.terms') }}">服務條款</a>
+            </li>
+        </ul>
+    </div>
+@endsection
