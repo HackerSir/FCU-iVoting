bd9b5f2c1be7792f2599afab1cef7658fcb3d349
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-08-12 14:03:40 +0800

    Make newest selection default to be the last one

--- app/Http/Controllers/VoteSelectionController.php
+++ app/Http/Controllers/VoteSelectionController.php
@@ -99,10 +99,11 @@ class VoteSelectionController extends Controller
             //$obj->image = explode(PHP_EOL, $request->get('image'));
             $obj->image = preg_split('/(\n|\r|\n\r)/', $request->get('image'), NULL, PREG_SPLIT_NO_EMPTY);
             $json = json_encode($obj);
-
+            $order = ($voteEvent->voteSelections->count() > 0) ? $voteEvent->voteSelections->max('order') : 0;
             $voteSelection = VoteSelection::create(array(
                 'vote_event_id' => $voteEvent->id,
-                'data' => $json
+                'data' => $json,
+                'order' => $order
             ));
             return Redirect::route('vote-event.show', $voteSelection->voteEvent->id)
                 ->with('global', '投票選項已建立');
