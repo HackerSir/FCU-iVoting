f52e6d52781f6e228f474235c0f596d40c5341e4
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-08-19 20:13:01 +0800

    Make navbar support open in new window

--- resources/views/common/navbar_item.blade.php
+++ resources/views/common/navbar_item.blade.php
@@ -1,7 +1,7 @@
-<li @if((Request::is($uri) && $uri!="/") || Request::route()->getPath()==$uri) class="active" @endif>
-    <a href="{{ URL::to($uri) }}" @if(strpos($uri,'://') !== false) target="_blank" @endif>
+<li @if((Request::is(str_replace('!','',$uri)) && $uri!="/") || Request::route()->getPath()==str_replace('!','',$uri)) class="active" @endif>
+    <a href="{{ URL::to(str_replace('!','',$uri)) }}" @if(strpos($uri,'://') !== false || starts_with($uri, '!')) target="_blank" @endif>
         {!! (Auth::check())?str_replace('%user%',Auth::user()->getNickname(),$name):$name !!}
-        @if(strpos($uri,'://') !== false)
+        @if(strpos($uri,'://') !== false || starts_with($uri, '!'))
             <i class="glyphicon glyphicon-new-window"></i>
         @endif
     </a>
