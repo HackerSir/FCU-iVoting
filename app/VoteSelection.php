d3ae1827a93ad84850611175667208f9dcb05c27
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-08-20 21:09:40 +0800

    Update title of vote selections

--- app/VoteSelection.php
+++ app/VoteSelection.php
@@ -20,30 +20,6 @@ class VoteSelection extends Model
         return $this->hasMany('App\VoteBallot');
     }
 
-    /** 利用Eloquent的Accessors & Mutators，可用$var->title取值或賦值
-     * 參考：http://laravel.tw/docs/5.1/eloquent-mutators#accessors-and-mutators
-     */
-    public function getTitleAttribute()
-    {
-        //如果有標題屬性，直接取標題
-        if (!empty($this->attributes['title'])) {
-            return $this->attributes['title'];
-        }
-        //以下為向下相容，自動更新舊資料
-        //若無標題屬性，嘗試從data解析標題
-        if (JsonHelper::isJson($this->data)) {
-            $json = json_decode($this->data);
-            if (!empty($json->title)) {
-                //找出並移除data中的title
-                $this->attributes['title'] = $json->title;
-                unset($json->title);
-                $this->data = json_encode($json, JSON_UNESCAPED_UNICODE);
-                $this->save();
-            }
-        }
-        return $this->attributes['title'];
-    }
-
     public function getImageLinksText()
     {
         return ($this->getImageLinks()) ? implode(PHP_EOL, $this->getImageLinks()) : null;
---
+++ database/migrations/2015_08_20_205221_update_title_of_vote_selection.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\VoteSelection;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateTitleOfVoteSelection extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $voteSelectionList = VoteSelection::all();
+        foreach ($voteSelectionList as $voteSelection) {
+            if (!empty($voteSelection->title)) {
+                continue;
+            }
+            //若無標題屬性，嘗試從data解析標題
+            $json = json_decode($voteSelection->data);
+            if (json_last_error() != JSON_ERROR_NONE) {
+                continue;
+            }
+            if (!empty($json->title)) {
+                //找出並移除data中的title
+                $voteSelection->title = $json->title;
+                unset($json->title);
+                $voteSelection->data = json_encode($json, JSON_UNESCAPED_UNICODE);
+                $voteSelection->save();
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $voteSelectionList = VoteSelection::all();
+        foreach ($voteSelectionList as $voteSelection) {
+            $json = json_decode($voteSelection->data);
+            if (json_last_error() != JSON_ERROR_NONE) {
+                continue;
+            }
+            //將title存入data
+            try {
+                $json->title = $voteSelection->title;
+                $voteSelection->title = "";
+                $voteSelection->data = json_encode($json, JSON_UNESCAPED_UNICODE);
+                $voteSelection->save();
+            } catch (Exception $ignore) {
+                //避免data不是json物件而是字串
+            }
+        }
+    }
+}
