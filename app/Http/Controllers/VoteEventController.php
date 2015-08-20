08ba49e8062669d03810502f7ea1b6d404c7b7ff
Chan, Danny <danny50610@yahoo.com.tw>
2015-08-20 19:22:00 +0800

    Remove unused import

--- app/Http/Controllers/VoteEventController.php
+++ app/Http/Controllers/VoteEventController.php
@@ -15,7 +15,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Log;
 
 class VoteEventController extends Controller
 {
