28e2c1dee335c7cd900be3fa407085e148114226
Chan, Danny <danny50610@yahoo.com.tw>
2015-08-23 20:47:56 +0800

    Let policies text larger

--- resources/views/policies.blade.php
+++ resources/views/policies.blade.php
@@ -11,7 +11,7 @@
             </li>
         </ul>
         <div class="panel panel-default">
-            <div class="panel-body">
+            <div class="panel-body" style="font-size: larger;">
                 @if(Request::is('policies/privacy'))
                     {!! App\MarkdownUtil::translate(File::get('privacy.md')) !!}
                 @elseif(Request::is('policies/terms'))
