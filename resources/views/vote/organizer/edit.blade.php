24890d441101b20bac46aa6d2de1f163a5bc1fd7
Chan, Danny <danny50610@yahoo.com.tw>
2015-08-25 14:29:13 +0800

    Remove unused css

--- resources/views/vote/organizer/edit.blade.php
+++ resources/views/vote/organizer/edit.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <div class="container" style="min-height: 600px">
+    <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="well bs-component">
