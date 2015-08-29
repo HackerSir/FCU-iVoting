c2b861db485377c345ea69070b5b17d19475caa6
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-08-29 16:11:12 +0800

    Fix typo

--- public/js/imgur-helper.js
+++ public/js/imgur-helper.js
@@ -6,6 +6,6 @@ function getImgurImageId(url) {
 function getImgurThumbnail(url, suffix) {
     var extensionReg = /[^\\\\]*\.(\w+)$/;
     var extension = extensionReg.exec(url)[1];
-    suffix = (typeof suffix === 'undefined' && $.inArray(suffix, ['s', 'b', 't', 'm', 'l', 'h'])) ? '' : suffix;
+    suffix = (typeof suffix === 'undefined' || !$.inArray(suffix, ['s', 'b', 't', 'm', 'l', 'h'])) ? '' : suffix;
     return '//i.imgur.com/' + getImgurImageId(url) + ((extension != "gif") ? suffix : '') + '.' + extension;
 }
