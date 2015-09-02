dcca285e4dabb6064e1201a1552efe4c8276145e
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-09-02 22:56:35 +0800

    Update stats page

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -10,7 +10,7 @@
             <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        統計（更新時間：<span title="更新時間：{{ (new Carbon($stats->time))->diffForHumans() }}<br />下次更新：{{ (new Carbon($stats->time))->addMinutes($cacheMinute)->diffForHumans() }}">{{ $stats->time }}</span>）
+                        統計（更新時間：<span title="更新時間：{{ (new Carbon($stats->time)) }}（{{ (new Carbon($stats->time))->diffForHumans() }}）<br />下次更新：{{ (new Carbon($stats->time))->addMinutes($cacheMinute) }}（{{ (new Carbon($stats->time))->addMinutes($cacheMinute)->diffForHumans() }}）">{{ $stats->time }}</span>）
                         {!! link_to_route('stats.force-renew','強制更新',null,['class' => 'btn btn-xs btn-primary', 'title' => '強制更新緩存資料']) !!}
                     </div>
                     <div class="panel-body">
