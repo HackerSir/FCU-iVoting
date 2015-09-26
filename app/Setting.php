8bac8d3112bddd8100a8779fa09473cb4114845b
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-09-26 22:22:26 +0800

    Add some comments

--- app/Setting.php
+++ app/Setting.php
@@ -9,6 +9,7 @@ class Setting extends Model
 {
     protected $table = 'settings';
     protected $fillable = ['id', 'type', 'desc', 'data'];
+    //有效型態與對應簡介
     public static $types = [
         'text' => '單行文字',
         'multiline' => '多行文字',
@@ -40,21 +41,27 @@ class Setting extends Model
         $setting->save();
     }
 
+    //取得型態
     public function getType()
     {
+        //檢查是否為有效型態
         if (!in_array($this->type, array_keys(static::$types))) {
+            //若不是，則自動選擇第一個型態
             return head(array_keys(static::$types));
         }
         return $this->type;
     }
 
+    //取得型態簡介文字
     public function getTypeDesc()
     {
         return static::$types[$this->getType()];
     }
 
+    //取得資料
     public function getData()
     {
+        //依照型態進行不同處理
         if ($this->getType() == 'text') {
             return htmlspecialchars($this->data);
         }
