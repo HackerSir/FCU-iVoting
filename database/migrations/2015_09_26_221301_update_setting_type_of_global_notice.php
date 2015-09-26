3514367ffa7502886a5a7055a75ed545e28ce02b
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-09-26 22:17:13 +0800

    Update setting type of global notice

---
+++ database/migrations/2015_09_26_221301_update_setting_type_of_global_notice.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSettingTypeOfGlobalNotice extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $setting = \App\Setting::find('global-notice');
+        $setting->type = 'markdown';
+        $setting->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $setting = \App\Setting::find('global-notice');
+        $setting->type = 'text';
+        $setting->save();
+    }
+}
