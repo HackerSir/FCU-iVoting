ba6cd633aa99657cea2774ea3d3b06598a103d25
Chan, Danny <danny50610@yahoo.com.tw>
2016-08-08 19:55:05 +0800

    Fix phpunit.xml

--- config/database.php
+++ config/database.php
@@ -60,17 +60,6 @@ return [
             'prefix' => '',
             'strict' => false,
         ],
-        'test_mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'database' => 'test_' . env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
