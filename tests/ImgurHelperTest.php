2361afa7bb804890fed5b040d6843bd37c7bd843
Chan, Danny <danny50610@yahoo.com.tw>
2016-08-25 14:01:24 +0800

    Better test

--- tests/ImgurHelperTest.php
+++ tests/ImgurHelperTest.php
@@ -11,25 +11,13 @@ class ImgurHelperTest extends TestCase
     public function test_getImgurID_function()
     {
         //測試 http:// 開頭
-        $this->assertEquals(
-            ImgurHelper::getImgurID('http://i.imgur.com/ABCDEF.jpg'),
-            'ABCDEF'
-        );
+        $this->assertEquals('ABCDEF', ImgurHelper::getImgurID('http://i.imgur.com/ABCDEF.jpg'));
         //測試 https:// 開頭
-        $this->assertEquals(
-            ImgurHelper::getImgurID('https://i.imgur.com/ABCDEF.jpg'),
-            'ABCDEF'
-        );
+        $this->assertEquals('ABCDEF', ImgurHelper::getImgurID('https://i.imgur.com/ABCDEF.jpg'));
         //測試 // 開頭
-        $this->assertEquals(
-            ImgurHelper::getImgurID('//i.imgur.com/ABCDEF.jpg'),
-            'ABCDEF'
-        );
+        $this->assertEquals('ABCDEF', ImgurHelper::getImgurID('//i.imgur.com/ABCDEF.jpg'));
         //測試 w.imgur.com
-        $this->assertEquals(
-            ImgurHelper::getImgurID('https://w.imgur.com/ABCDEF.jpg'),
-            'ABCDEF'
-        );
+        $this->assertEquals('ABCDEF', ImgurHelper::getImgurID('https://w.imgur.com/ABCDEF.jpg'));
     }
 
     /**
@@ -42,23 +30,23 @@ class ImgurHelperTest extends TestCase
     {
         //測試 錯誤網址
         $this->assertEquals(
-            ImgurHelper::thumbnail('http://i.imgur.com/'),
-            'http://i.imgur.com/'
+            'http://i.imgur.com/',
+            ImgurHelper::thumbnail('http://i.imgur.com/')
         );
         //測試 正常網址
         $this->assertEquals(
-            ImgurHelper::thumbnail('http://i.imgur.com/ABCDEF.jpg'),
-            '//i.imgur.com/ABCDEF.jpg'
+            '//i.imgur.com/ABCDEF.jpg',
+            ImgurHelper::thumbnail('http://i.imgur.com/ABCDEF.jpg')
         );
         //測試 縮圖功能
         $this->assertEquals(
-            ImgurHelper::thumbnail('http://i.imgur.com/ABCDEF.jpg', 'l'),
-            '//i.imgur.com/ABCDEFl.jpg'
+            '//i.imgur.com/ABCDEFl.jpg',
+            ImgurHelper::thumbnail('http://i.imgur.com/ABCDEF.jpg', 'l')
         );
         //測試 副檔名保留
         $this->assertEquals(
-            ImgurHelper::thumbnail('http://i.imgur.com/ABCDEF.gif', 'l'),
-            '//i.imgur.com/ABCDEFl.gif'
+            '//i.imgur.com/ABCDEFl.gif',
+            ImgurHelper::thumbnail('http://i.imgur.com/ABCDEF.gif', 'l')
         );
         //測試 縮圖功能，但是縮圖參數錯誤
         $this->assertNull(
