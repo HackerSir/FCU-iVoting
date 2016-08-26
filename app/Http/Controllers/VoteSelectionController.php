7984c4fdb895090bf5bd6d8508318b7077c8cac8
KID <jyhsu2000@yahoo.com.tw>
2016-08-26 18:12:15 +0800

    Remove unnecessary code

--- app/Http/Controllers/VoteSelectionController.php
+++ app/Http/Controllers/VoteSelectionController.php
@@ -201,11 +201,6 @@ class VoteSelectionController extends Controller
 
     public function vote(VoteSelection $voteSelection)
     {
-        if (!$voteSelection->voteEvent) {
-            return redirect()->route('voteEvent.show', $voteSelection->voteEvent)
-                ->with('warning', '投票活動不存在');
-        }
-
         if (!$voteSelection->voteEvent->isInProgress()) {
             return redirect()->route('voteEvent.show', $voteSelection->voteEvent)
                 ->with('warning', '非投票期間');
