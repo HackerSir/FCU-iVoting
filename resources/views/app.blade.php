042c726cc33dbeb5696af6b136040479b719245d
KID <jyhsu2000@yahoo.com.tw>
2016-08-31 23:52:44 +0800

    Add Google Analysis

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -96,10 +96,6 @@
         {!! HTML::script('js/bootstrap-notify.min.js') !!}
         {!! HTML::script('js/notify.js') !!}
 
-        @if(App::environment('production'))
-            {!! HTML::script('js/analyticstracking.js') !!}
-        @endif
-
         @yield('javascript')
 
         <script type="text/javascript">
@@ -123,6 +119,23 @@
                     offset: { x: 0, y: 15 },
                     stem: false
                 });
+
+                // Google分析
+                @if(env('GOOGLE_ANALYSIS'))
+                (function (i, s, o, g, r, a, m) {
+                    i['GoogleAnalyticsObject'] = r;
+                    i[r] = i[r] || function () {
+                            (i[r].q = i[r].q || []).push(arguments)
+                        }, i[r].l = 1 * new Date();
+                    a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                    a.async = 1;
+                    a.src = g;
+                    m.parentNode.insertBefore(a, m)
+                })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+                ga('create', '{{ env('GOOGLE_ANALYSIS') }}', 'auto');
+                ga('send', 'pageview');
+                @endif
             });
         </script>
     </body>
