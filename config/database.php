d05c5e3498153368cf94b9662cb65622b506970a
KID <jyhsu2000@yahoo.com.tw>
2016-08-31 07:32:51 -0500

    Applied fixes from StyleCI (#22)
    [ci skip] [skip ci]

--- config/database.php
+++ config/database.php
@@ -107,9 +107,9 @@ return [
     'redis' => [
         'cluster' => false,
         'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
+            'host'     => env('REDIS_HOST', 'localhost'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            'port'     => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
     ],
