e4a7ce055fd366062a04ef8adbe241ce9297fa37
KID <jyhsu2000@yahoo.com.tw>
2016-08-31 13:28:40 +0800

    Update error page

--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -1,17 +1,55 @@
-@extends('app')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ $code }} - {{ $message }}</title>
 
-@section('title')
-    {{ $code }}
-@endsection
+    <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-@section('content')
-    <div class="container">
-        <div class="row text-center">
-            <h1>{{ $code }}</h1>
-            <h1>{{ $message }}</h1>
-            @if(!empty($pic))
-                {!! HTML::image($pic, $message, ['width' => '600px']) !!}
-            @endif
-        </div>
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+
+        .subtitle {
+            font-size: 48px;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">{{ $code }}</div>
+        <div class="subtitle">{{ $message }}</div>
+        <a href="{{ url('/') }}">返回首頁</a><br/>
+        <a href="javascript:history.back()">返回上一頁</a>
     </div>
-@endsection
+</div>
+</body>
+</html>
