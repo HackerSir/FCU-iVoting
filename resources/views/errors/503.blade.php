72359398990c057d56c421ac5ed7fd7fdc76289a
KID <jyhsu2000@yahoo.com.tw>
2016-09-01 21:33:59 +0800

    Update 503 page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,74 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Be right back.</title>
+<head>
+    <title>系統維護中</title>
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <style>
+        html, body {
+            height: 100%;
+        }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+        body:after {
+            content: "";
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            background: url(http://i.imgur.com/ZYyi4v5.png) no-repeat fixed center;
+            width: 100%;
+            height: 100%;
+            opacity: 0.2;
+            z-index: -1;
+        }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato', sans-serif;
+        }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+        a {
+            text-decoration: none;
+        }
 
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
-            </div>
-        </div>
-    </body>
+        a:hover {
+            text-decoration: underline;
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+
+        .subtitle {
+            font-size: 48px;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">系統維護中</div>
+        此時您可以前往...<br/>
+        <a href="https://hackersir.org" class="subtitle">逢甲大學黑客社</a>
+    </div>
+</div>
+</body>
 </html>
